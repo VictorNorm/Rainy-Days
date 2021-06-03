b5773ee9df6e5b8472cddf1d921a5eb1dd19388d
VictorNorm <VictorNorm@users.noreply.github.com>
2021-06-03 21:42:11 +0200

    updated

--- script/index.js
+++ script/index.js
@@ -1,13 +1,16 @@
 window.onload = function () {
 
     // Cart functionality ---------------------------------------------------------//
+    
     function loadFromStorage(itemName) {
         const itemString = localStorage.getItem(itemName);
         let item = JSON.parse(itemString);
+        console.log(item)
         return item
     }
+    
 
-    function numberOfProductschecker(array, number, object) {
+    function numberOfProductsChecker(array, number, object) {
         if (array.length !== number) {
             object.innerHTML = array.length;
 
@@ -15,11 +18,21 @@ window.onload = function () {
             object.innerHTML = number;
         }
     }
-
+    /*
+    function numberOfProductsChecker(array, number, object) {
+        if (array === null) {
+            object.innerHTML = number;
+            myProducts;
+            console.log(myProducts);
+        } else if (array.length === null) {
+            object.innerHTML = number;
+        }
+    }
+*/
     const numberOfProductsInCart = document.querySelector(".numberOfProductsInCart");
-    const myProducts = loadFromStorage('chosenProducts');
+    let myProducts = loadFromStorage('chosenProducts') ?? [];
 
-    numberOfProductschecker(myProducts, 0, numberOfProductsInCart);
+    numberOfProductsChecker(myProducts, 0, numberOfProductsInCart);
 
 
     // Api call and HTML generation -------------------------------------------//
